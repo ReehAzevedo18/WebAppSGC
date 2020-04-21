cf22c21d1220396454ef4e2a98bdd30c95671c4d
ReehAzevedo18 <58194615+ReehAzevedo18@users.noreply.github.com>
2020-04-20 21:08:12 -0300

    Projeto Inicial

---
+++ src/SGC.Infrastructure/Data/ClienteContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGC.ApplicationCore.Enity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.Infrastructure.Data
+{
+    public class ClienteContext : DbContext //herdar a classe
+    {
+        /*passar o contexto qual a classe ta usando*/
+        public ClienteContext(DbContextOptions<ClienteContext> options)  : base(options) //classe base é o DBContext
+        {
+
+        }
+
+
+
+        //Criando uma tabela no banco
+        public DbSet<Cliente> Clientes { get; set; }
+
+        //Configuração do EnityFramework
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Tb_cliente"); //informando ao EF como será a nomenclatura da tabela
+        }
+    }
+}
